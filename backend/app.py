6a8ecdc8913d51f299c10ee5722a659481aaf008
Joaquin <joaquin04murillo@gmail.com>
2026-04-25 16:23:52 -0300

    fix todos los links navbar

--- backend/app.py
+++ backend/app.py
@@ -29,7 +29,7 @@ def cargar_usuarios():
     with open("user.json", "r") as archivo:
         return json.load(archivo)
 
-@app.route("/index.html")
+@app.route("/index")
 def index():
     return render_template("index.html")
 
